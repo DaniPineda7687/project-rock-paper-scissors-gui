477a27132e9e423ac0ab9b44b4f7bceca180e878
Daniel Pineda <danielspineda87@gmail.com>
2021-06-25 16:33:53 -0500

    Rock, paper or scissors project, with graphic interface

---
+++ scripts/main.js
@@ -0,0 +1,101 @@
+const option1= "paper";
+const option2= "rock";
+const option3= "scissors";
+const options = document.querySelectorAll(".player-options input");
+const userPointsShow = document.querySelector(".user-points");
+const machinePointsShow = document.querySelector(".machine-points");
+const computerOptionShow = document.querySelector(".computer-option");
+const imageComputer = document.querySelector("#option-computer");
+const results = document.querySelector(".results #round-result");
+const finishResult = document.querySelector(".results #finish-result");
+const buttonRetry = document.querySelector(".retry");
+
+let userPoints=0;
+let computerPoints=0;
+let userOption;
+let count=0;
+buttonRetry.style.display="none" 
+const computerPlay=()=>{
+    let options = ["rock","paper","scissors"];
+    let optionComputer = Math.floor(Math.random()*3);
+    return options[optionComputer];   
+}
+
+options.forEach(option=>option.addEventListener("click",(e)=>{
+    if(count<5){
+        userOption = e.target.id;
+        machineOption = computerPlay();
+        writeEffect(results,playRound(userOption,machineOption));
+        imageComputer.setAttribute("src",`../resources/${machineOption}.jpg`);
+        userPointsShow.textContent=`User points: ${userPoints}`;
+        machinePointsShow.textContent=`Machine points: ${computerPoints}`;
+        results.textContent="";
+        count++;
+        isFinish();
+    }
+}));
+
+function isFinish(){
+    if(count==5){
+        if(userPoints==computerPoints){
+            writeEffect(finishResult,`Its a tie ${userPoints}-${computerPoints}`);
+        }else if(userPoints>computerPoints){
+            writeEffect(finishResult,`You win! ${userPoints}-${computerPoints}`);
+        }else{
+            writeEffect(finishResult,`You lose! ${userPoints}-${computerPoints}`);
+        }
+        buttonRetry.style.display="block" 
+    }
+}
+
+function playRound(userSelection, pcSelection){
+        if(userSelection==pcSelection){
+            userPoints+=0;
+            computerPoints+=0;
+            return `It's a tie`;          
+        }else if((userSelection==option1)&&(pcSelection==option2)){
+            userPoints+=1;
+            computerPoints+=0;
+            return `You Win! ${userSelection} beats ${pcSelection}`;
+        }else if((userSelection)==option1&&(pcSelection==option3)){
+            userPoints+=0;
+            computerPoints+=1;
+            return `You Lose! ${pcSelection} beats ${userSelection}`;
+        }else if((userSelection)==option2&&(pcSelection==option1)){
+            userPoints+=0;
+            computerPoints+=1;
+            return `You Lose! ${pcSelection} beats ${userSelection}`;
+        }else if((userSelection)==option2&&(pcSelection==option3)){
+            userPoints+=1;
+            computerPoints+=0;
+            return `You Win! ${userSelection} beats ${pcSelection}`;
+        }else if((userSelection)==option3&&(pcSelection==option1)){
+            userPoints+=1;
+            computerPoints+=0;
+            return `You Win! ${userSelection} beats ${pcSelection}`;
+        }else if((userSelection)==option3&&(pcSelection==option2)){
+            userPoints+=0;
+            computerPoints+=1;
+            return `You Lose! ${pcSelection} beats ${userSelection}`;
+        }
+}
+
+function writeEffect(component,sentence){
+    let words = sentence.split('');
+    let i=0;
+    let x = setInterval(()=>{
+        component.textContent+=words[i];
+        i++;
+        if(i==words.length){
+            clearInterval(x);
+        }
+    },90)
+}
+buttonRetry.addEventListener("click",()=>{
+    location.reload();
+});
+
+
+
+
+
